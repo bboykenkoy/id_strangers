087d3fbf8b796b5349b2169c15385e7df6eba0ce
Dat <dat12a21996@gmail.com>
2017-11-21 17:58:44 +0700

    123
    123

--- index.js
+++ index.js
@@ -34,7 +34,6 @@ server.timeout = 60000;
 
 
 io.on('connection', function(socket) {
-    console.log(socket.id)
     // --------------------------
     // VERIFY WHEN CONNECT SOCKET
     // --------------------------
