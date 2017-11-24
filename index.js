e0c960dfd5713b178a297fcd689f5b3708dfdaa3
Dat <dat12a21996@gmail.com>
2017-11-24 11:58:26 +0700

    123
    123

--- index.js
+++ index.js
@@ -74,10 +74,12 @@ io.on('connection', function(socket) {
     socket.on('seen', function(chat) {
         if (typeof chat == 'object' && chat.conversations_id && chat.id) {
             var sqlMess = "SELECT * FROM `message_status` WHERE `conversations_id`="+chat.conversations_id+" AND `users_id`="+chat.id;
+            console.log(sqlMess);
             APP.getObjectWithSQL(sqlMess, function(statusMessage){
                 if (statusMessage) {
                     var sql = "UPDATE `message_status` SET `status`=3 WHERE `conversations_id`="+chat.conversations_id+" AND `users_id`="+chat.id+"";
                     client.query(sql);
+                    console.log(sql);
                 }
             });
         }
