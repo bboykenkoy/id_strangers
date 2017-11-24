ef115470645abc5a7121f7171484e2adfa7d00b2
Dat <dat12a21996@gmail.com>
2017-11-24 11:51:07 +0700

    123
    123

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ io.on('connection', function(socket) {
     socket.on('seen', function(chat) {
         if (typeof chat == 'object' && chat.conversations_id && chat.id) {
             var sql = "UPDATE `message_status` SET `status`=3 WHERE `conversations_id`="+chat.conversations_id+" AND `users_id`="+chat.id;
-
+            console.log(sql);
         }
     });
     // --------------------------
