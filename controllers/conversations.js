57eaee8cec573c8acdd276dd91816d41c5b2bfbb
Dat <dat12a21996@gmail.com>
2017-11-24 11:19:56 +0700

    123
    123

--- controllers/conversations.js
+++ controllers/conversations.js
@@ -26,10 +26,12 @@ router.get('/:conversations_id/type=messages', parser, function(req, res) {
             APP.getObjectWithSQL(userSQL, function(data) {
                 if (data) {
                     async.forEachOf(data, function(element, i, callback) {
-                        var sttSQL = "SELECT `status`,`users_id` FROM `message_status` WHERE `messages_id`=" + element.id;
+                        var sttSQL = "SELECT `status` FROM `message_status` WHERE `messages_id`=" + element.id + " AND `users_id`=" + id + "";
                         APP.getObjectWithSQL(sttSQL, function(status) {
                             if (status) {
-                                data[i].status = status;
+                                data[i].status = status[0].status;
+                            } else {
+                                data[i].status = 0;
                             }
                             data[i].sender_id = data[i].users_id;
                             delete data[i].users_id;
